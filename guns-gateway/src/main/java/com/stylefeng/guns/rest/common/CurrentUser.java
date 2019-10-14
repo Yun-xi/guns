5e0b4a73bde95b1f04db0daada77c2554700ff0a
XieYaqi <987159036@qq.com>
2019-10-14 23:06:18 +0800

    userId放入threadlocal

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/common/CurrentUser.java
@@ -0,0 +1,9 @@
+package com.stylefeng.guns.rest.common;
+
+/**
+ * @author xieyaqi
+ * @mail 987159036@qq.com
+ * @date 2019-10-14 22:39
+ */
+public class CurrentUser {
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/vo/ResponseVO.java
@@ -0,0 +1,9 @@
+package com.stylefeng.guns.rest.modular.vo;
+
+/**
+ * @author xieyaqi
+ * @mail 987159036@qq.com
+ * @date 2019-10-14 22:32
+ */
+public class ResponseVO {
+}
