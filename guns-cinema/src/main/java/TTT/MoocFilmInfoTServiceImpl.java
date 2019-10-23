323770d3a70d74c3c575f9f53100a35e098b9bbe
xieyaqi <987159036@qq.com>
2019-10-23 16:57:56 +0800

    初始化 cinema 模块

---
+++ guns-cinema/src/main/java/TTT/MoocFilmInfoTServiceImpl.java
@@ -0,0 +1,20 @@
+package TTT;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmInfoT;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocFilmInfoTMapper;
+import TTT.IMoocFilmInfoTService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 影片主表 服务实现类
+ * </p>
+ *
+ * @author xieyaqi
+ * @since 2019-10-22
+ */
+@Service
+public class MoocFilmInfoTServiceImpl extends ServiceImpl<MoocFilmInfoTMapper, MoocFilmInfoT> implements IMoocFilmInfoTService {
+	
+}
