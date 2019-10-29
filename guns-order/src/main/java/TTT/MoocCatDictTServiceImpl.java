345af6008f4d1d887b9409bee4542d69ee4b8f62
xieyaqi <987159036@qq.com>
2019-10-29 15:55:17 +0800

    新增订单服务

---
+++ guns-order/src/main/java/TTT/MoocCatDictTServiceImpl.java
@@ -0,0 +1,20 @@
+package TTT;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocCatDictT;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocCatDictTMapper;
+import TTT.IMoocCatDictTService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 类型信息表 服务实现类
+ * </p>
+ *
+ * @author xieyaqi
+ * @since 2019-10-22
+ */
+@Service
+public class MoocCatDictTServiceImpl extends ServiceImpl<MoocCatDictTMapper, MoocCatDictT> implements IMoocCatDictTService {
+	
+}
